f370b67906cf967b2993ee22a32e9ab648dfd674
lanshuijuntuan <724598042@qq.com>
2018-12-23 23:05:00 +0800

    test 12

---
+++ page_demo/page_demo/Page_demos/testApplication.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="testApplication.aspx.cs" Inherits="page_demo.Page_demos.testApplication" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Test Application</title>
+</head>
+<body>
+    <h1>Test Application</h1>
+    <form id="form1" runat="server">
+    <div>
+    Hit Counter:<asp:Label ID="lbl_hitCounter" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ page_demo/page_demo/Page_demos/testApplication.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace page_demo.Page_demos
+{
+    public partial class testApplication : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Object hitCountObj= this.Application.Get("hitCounter");
+            if (hitCountObj == null)
+            {
+                this.Application.Add("hitCounter", 1);
+            }
+            else
+            {
+                int hitCount = (int)hitCountObj;
+                hitCount++;
+                this.Application.Set("hitCounter", hitCount);
+            }
+            this.lbl_hitCounter.Text = this.Application.Get("hitCounter").ToString();
+
+        }
+    }
+}
