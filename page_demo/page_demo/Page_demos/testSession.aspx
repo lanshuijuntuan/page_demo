c6febbd8a6fc68e8aea9c1d6137ab511d6ba9bd2
lanshuijuntuan <724598042@qq.com>
2018-12-23 23:47:39 +0800

    1356466

---
+++ page_demo/page_demo/Page_demos/testSession.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="testSession.aspx.cs" Inherits="page_demo.Page_demos.testSession" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Test Session</title>
+</head>
+<body>
+    <h1>Test Session</h1>
+    <form id="form1" runat="server">
+    <div>
+    Session Hit Counter:<asp:Label ID="lbl_sessionCount" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ page_demo/page_demo/Page_demos/testSession.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace page_demo.Page_demos
+{
+    public partial class testSession : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            object sessionCountObj=this.Session.Contents["sessionCount"];
+            if (sessionCountObj == null)
+            {
+                this.Session.Contents.Add("sessionCount", 1);
+            }
+            else
+            {
+                int result=0;
+                if (int.TryParse(sessionCountObj.ToString(), out result))
+                {
+                    this.Session.Contents["sessionCount"] = result + 1;
+                }
+                else
+                {
+                    this.Session.Contents.Remove("sessionCount");
+                    this.Session.Contents.Add("sessionCount", 1);
+                }
+            }
+            this.lbl_sessionCount.Text = this.Session.Contents["sessionCount"].ToString();
+
+        }
+    }
+}
